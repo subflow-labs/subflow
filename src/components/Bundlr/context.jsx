0c2e44ab331758679474053cb12e7c3efb188907
crustydev <ademola1145@gmail.com>
2022-12-04 02:43:57 +0100

    Refactor Bundlr context

--- src/components/Bundlr/context.jsx
+++ src/components/Bundlr/context.jsx
@@ -2,8 +2,18 @@ import { WebBundlr } from '@bundlr-network/client';
 import { useToast } from '@chakra-ui/react';
 import BigNumber from 'bignumber.js';
 import { providers, utils } from 'ethers';
-import React, { createContext, useContext, useEffect, useState } from 'react'
-const BundlrContext = createContext<IBundlrHook>({
+import React, { createContext, useContext, useEffect, useState } from 'react';
+
+/*
+export interface IBundlrHook {
+    initialiseBundlr: () => Promise<void>;
+    fundWallet: (_: number) => void;
+    balance: string;
+    uploadFile: (file: Buffer) => Promise<any>;
+    bundlrInstance: WebBundlr;
+}*/
+
+const BundlrContext = createContext({
     initialiseBundlr: async () => { },
     fundWallet: (_) => { },
     balance: '',
@@ -15,7 +25,8 @@ const BundlrContextProvider = ({ children }) => {
     const toast = useToast()
     //const [bundlrInstance, setBundlrInstance] = useState<WebBundlr>();
     const [bundlrInstance, setBundlrInstance] = useState();
-    const [balance, setBalance] = useState<string>('');
+    //const [balance, setBalance] = useState<string>('');
+    const [balance, setBalance] = useState("");
     useEffect(() => {
         if (bundlrInstance) {
             fetchBalance();
@@ -31,12 +42,13 @@ const BundlrContextProvider = ({ children }) => {
             provider,
             {
                 providerUrl:
-                    process.env.NEXT_PUBLIC_ALCHEMY_RPC_URL,
+                    process.env.ALCHEMY_MUMBAI_URL,
             }
         );
         await bundlr.ready();
         setBundlrInstance(bundlr);
     }
+
     async function fundWallet(amount) {
         try {
             if (bundlrInstance) {
@@ -78,6 +90,7 @@ const BundlrContextProvider = ({ children }) => {
             return conv
         }
     }
+
     async function fetchBalance() {
         if (bundlrInstance) {
             const bal = await bundlrInstance.getLoadedBalance();
@@ -85,6 +98,7 @@ const BundlrContextProvider = ({ children }) => {
             setBalance(utils.formatEther(bal.toString()));
         }
     }
+
     async function uploadFile(file) {
         try {
             let tx = await bundlrInstance.uploader.upload(file, [{ name: "Content-Type", value: "image/png" }])
