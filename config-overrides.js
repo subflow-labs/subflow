d9b12076fa5d4351b7d442c89492a6ad330b0c06
crustydev <ademola1145@gmail.com>
2022-12-21 23:45:34 +0100

    Resolve webpack5 fallbacks

--- config-overrides.js
+++ config-overrides.js
@@ -17,20 +17,22 @@ module.exports = function override(config) {
     "fs": false, 
     }) 
  config.resolve.fallback = fallback;
- config.resolve.extensions = [...config.resolve.extensions, ".ts", ".js"] 
+ //config.resolve.extensions = [...config.resolve.extensions, ".ts", ".js"] 
+ /*
  config.plugins = (config.plugins || []).concat([ 
    new webpack.ProvidePlugin({ 
     process: 'process/browser', 
     Buffer: ['buffer', 'Buffer'] 
   }) 
- ]) 
+ ]) */
 
+ /*
  config.module.rules.push({
   test: /\.m?js/,
   resolve: {
       fullySpecified: false
   }
-})
+})*/
 
  return config; }
 
